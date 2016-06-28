d9c2c75c02ada9407e9feb01efd31b1008cb887c
lixiaohu1990 <245213009@qq.com>
2016-06-28 10:43:36 +0800

    day 2

--- Day_01_View/index.ios.js
+++ Day_01_View/index.ios.js
@@ -12,9 +12,95 @@ import {
   Text,
   View,
   PixelRatio,
+  TouchableWithoutFeedback,
 } from 'react-native';
 
+class ViewBorderStyleExample extends Component{
+    constructor(props){
+      super(props);
+      this.state = {
+        showBorder: true,
+      };
+    }
 
+    _handlePress() {
+    this.setState({showBorder: !this.state.showBorder});
+    };
+
+    render() {
+      return (
+        <TouchableWithoutFeedback onPress={this._handlePress.bind(this)}>
+          <View>
+
+            <View style={[styles.center,
+              {
+              marginTop: 20,
+              height:40,
+              borderWidth: 1,
+              borderStyle: this.state.showBorder ? 'dashed' : 'dotted',
+              margin: 5,
+              backgroundColor: this.state.showBorder ? '#FF0067' : '#527FE4',
+              }]}>
+
+              <Text style={styles.font}>borderStyle</Text>
+
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      );
+    }
+
+  };
+
+class ZIndexExample extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      flipped: false,
+    };
+  }
+
+  render() {
+    const indices = this.state.flipped ? [-1, 0, 1, 2] : [2, 1, 0, -1];
+    return (
+      <TouchableWithoutFeedback onPress={this._handlePress.bind(this)}>
+        <View style = {{margin: 10}}>
+          <Text style={{paddingBottom: 10}}>Tap to flip sorting order</Text>
+          <View style={[
+            styles.myzIndex,
+            {marginTop: 0, backgroundColor: '#E57373', elevation: indices[0]}
+          ]}>
+            <Text>ZIndex {indices[0]}</Text>
+          </View>
+          <View style={[
+            styles.myzIndex,
+            {marginLeft: 50, backgroundColor: '#FFF176', elevation: indices[1]}
+          ]}>
+            <Text>ZIndex {indices[1]}</Text>
+          </View>
+          <View style={[
+            styles.myzIndex,
+            {marginLeft: 100, backgroundColor: '#81C784', elevation: indices[2]}
+          ]}>
+            <Text>ZIndex {indices[2]}</Text>
+          </View>
+          <View style={[
+            styles.myzIndex,
+            {marginLeft: 150, backgroundColor: '#64B5F6', elevation: indices[3]}
+          ]}>
+            <Text>ZIndex {indices[3]}</Text>
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    );
+  }
+  
+  _handlePress() {
+    this.setState({flipped: !this.state.flipped});
+  }
+};
+
+  
 
 class Day_01_View extends Component {
   render() {
@@ -37,9 +123,6 @@ class Day_01_View extends Component {
             <Text style={styles.font}>特惠酒店</Text>
 
           </View>
-
-
-
         </View>
 
 
@@ -60,15 +143,51 @@ class Day_01_View extends Component {
 
         </View>
       </View>
+        <ViewBorderStyleExample/>
 
+        <View style={{borderRadius: 10, borderWidth: 1, width: 20, height: 20, margin: 10,}} />
+          
+        <View style={{flexDirection: 'row', margin: 10,}}>
+          <View
+            style={{
+              width: 95,
+              height: 10,
+              marginRight: 10,
+              marginBottom: 5,
+              overflow: 'hidden',
+              borderWidth: 0.5,
+            }}>
+            <View style={{width: 200, height: 20}}>
+              <Text>Overflow hidden</Text>
+            </View>
+          </View>
+          <View style={{width: 95, height: 10, marginBottom: 5, borderWidth: 0.5}}>
+            <View style={{width: 200, height: 20}}>
+              <Text>Overflow visible</Text>
+            </View>
+          </View>
         </View>
+
+        <View style = {{margin: 10}}>
+          <View style={{opacity: 0}}><Text>Opacity 0</Text></View>
+          <View style={{opacity: 0.1}}><Text>Opacity 0.1</Text></View>
+          <View style={{opacity: 0.3}}><Text>Opacity 0.3</Text></View>
+          <View style={{opacity: 0.5}}><Text>Opacity 0.5</Text></View>
+          <View style={{opacity: 0.7}}><Text>Opacity 0.7</Text></View>
+          <View style={{opacity: 0.9}}><Text>Opacity 0.9</Text></View>
+          <View style={{opacity: 1}}><Text>Opacity 1</Text></View>
+        </View>
+
+        <ZIndexExample/>
+        </View>
+
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    marginTop:200,
+    marginTop:50,
     marginLeft:5,
     marginRight:5,
     height:84,
@@ -102,6 +221,12 @@ const styles = StyleSheet.create({
   lineCenter:{
     borderBottomWidth:1/PixelRatio.get(),
     borderColor:'#fff',
+  },
+  myzIndex: {
+    justifyContent: 'space-around',
+    width: 100,
+    height: 50,
+    marginTop: -10,
   },
 });
 
---
+++ Demo_04_2_QQLogin/index.ios.js
@@ -0,0 +1,122 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+'use strict'
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Image,
+} from 'react-native';
+
+class Demo_04_2_QQLogin extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image style={styles.image}
+               source={require('./img/app_icon.png')}
+        />
+
+        <TextInput style={styles.userInput}
+                   placeholder='QQ号/手机号/邮箱'
+                   numberOfLines = {1}
+                   autoFocus= {true}
+                   textAligin = 'center'
+         >
+        </TextInput>
+
+        <View style={{backgroundColor:'#f4f4f4', height:1}}/>
+
+        <TextInput style={styles.userPassword}
+                   placeholder='密码'
+                   numberOfLines = {1}
+                   secureTextEntry={true}
+                   keyboardType = 'numeric'
+        ></TextInput>
+
+        <View style={styles.viewCommit}>
+          <Text style={{color:"#fff"}}>登录</Text>
+        </View>
+
+        <View style={styles.viewBottom}>
+          <Text style={styles.viewUnlogin}>无法登录?</Text>
+          <Text style={styles.viewRegister}>新用户</Text>
+
+        </View>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f4f4f4',
+    padding:10,
+  },
+
+  image: {
+    borderRadius: 35,
+    height: 70,
+    width: 70,
+    marginTop: 100,
+    alignSelf: 'center',
+  },
+
+  userInput: {
+    marginTop:10,
+    backgroundColor:'#fff',
+    height:40,
+    padding: 10,
+  },
+
+  userPassword: {
+    height: 40,
+    backgroundColor:'#fff',
+    padding: 10
+  },
+
+  viewCommit: {
+    marginTop: 15,
+    marginLeft: 10,
+    marginRight: 10,
+    backgroundColor: '#63B8FF',
+    height: 35,
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  viewBottom: {
+    flex: 1,
+    flexDirection:'row',
+    bottom: 10,
+    alignItems:'flex-end',
+
+
+  },
+
+  viewUnlogin: {
+    fontSize: 12,
+    color: '#63B8FF',
+    marginLeft: 10,
+  },
+
+  viewRegister: {
+    fontSize: 12,
+    color:'#63B8FF',
+    marginRight:10,
+    alignItems:'flex-end',
+    flex:1,
+    flexDirection:'row',
+    textAlign: 'right',
+  }
+});
+
+AppRegistry.registerComponent('Demo_04_2_QQLogin', () => Demo_04_2_QQLogin);
